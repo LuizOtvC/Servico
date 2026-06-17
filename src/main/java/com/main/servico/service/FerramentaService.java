050c121251759b1be8571e905f1e4995205c1d3d
Luiz Otávio <Luiz.fontes0@outlook.com>
2026-06-17 17:06:37 -0300

    service

--- src/main/java/com/main/servico/service/FerramentaService.java
+++ src/main/java/com/main/servico/service/FerramentaService.java
@@ -8,7 +8,9 @@ import com.main.servico.model.Ferramenta;
 import com.main.servico.repository.FerramentaRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 /**
  *
@@ -25,13 +27,40 @@ public class FerramentaService {
     }
     
     public void register(Ferramenta tool){
-        service.register(tool);
+        if(tool.getNome().equals("")){
+            throw new ResponseStatusException(HttpStatusCode.valueOf(400), "Nome não encontrado");
+        }
+        if(tool.getVidaUtilMaxima() <= 0){
+            throw new ResponseStatusException(HttpStatusCode.valueOf(400), "Vida util não encontrada");
+        }
+        if(tool.getHorasDeUso() < 0){
+            throw new ResponseStatusException(HttpStatusCode.valueOf(400), "horas de uso não encontradas");
+        }
+        int linhas = service.register(tool);
+        if(linhas == 0){
+            throw new ResponseStatusException(HttpStatusCode.valueOf(500), "Erro ao adicionar ao banco de dados");
+        }
     }
     public void deletar(Long id){
-        service.deletar(id);
+        int linhas = service.deletar(id);
+        if(linhas == 0){
+            throw new ResponseStatusException(HttpStatusCode.valueOf(500), "Erro ao deletar do banco de dados");
+        }
     }
-    public void Salvar(Ferramenta ferramenta){
-        service.Salvar(ferramenta);
+    public void Salvar(Ferramenta tool){
+        if(tool.getNome().equals("")){
+            throw new ResponseStatusException(HttpStatusCode.valueOf(400), "Nome não encontrado");
+        }
+        if(tool.getVidaUtilMaxima() <= 0){
+            throw new ResponseStatusException(HttpStatusCode.valueOf(400), "Vida util não encontrada");
+        }
+        if(tool.getHorasDeUso() < 0){
+            throw new ResponseStatusException(HttpStatusCode.valueOf(400), "horas de uso não encontradas");
+        }
+        int linhas = service.Salvar(tool);
+        if(linhas == 0){
+            throw new ResponseStatusException(HttpStatusCode.valueOf(500), "Erro ao atualizar o banco de dados");
+        }
     }
     
     public Ferramenta buscarPorId(Long id) {
